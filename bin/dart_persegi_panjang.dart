35496b70e34afb616b15a8a9b51b0bcf4dd9e6c1
Alfathinanda <alfathjs@mhs.mdp.ac.id>
2023-10-11 17:53:53 +0700

    Update dart_persegi_panjang.dart

--- bin/dart_persegi_panjang.dart
+++ bin/dart_persegi_panjang.dart
@@ -1,5 +1,15 @@
-import 'package:dart_persegi_panjang/dart_persegi_panjang.dart' as dart_persegi_panjang;
+import 'package:dart_persegi_panjang/dart_persegi_panjang.dart'
+    as dart_persegi_panjang;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_persegi_panjang.calculate()}!');
+
+  int luasPersegiPanjang() {
+    return panjang * lebar;
+  }
+
+  //fungsi keliling persegi panjang
+  int kelilingPersegiPanjang(){{
+  return 2 *(panjang + lebar)};
+  }
 }
